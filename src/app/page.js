88106e5b5cbeba91695436f2ccb93b88bf4fad33
SizoDevelops <fierceincii@gmail.com>
2023-11-19 22:16:22 +0200

    Yoh

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,8 @@ import { useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation';
 import { useEffect } from "react";
 const Page = () => {
-  const router = useRouter();
  const {data: session, status} = useSession()
-  useEffect(() => {
-    if (status !== "loading" && !session) {
-      // Redirect the user to the login page if not authenticated
-      router.push('/'); // Replace '/login' with your login page route
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [status, session]);
-
+ 
   if (status === "loading") {
     return <Loader />;
   } else if (session) {
