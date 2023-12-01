8d7261f47771088054a632a604fd5af8edcefce6
SizoDevelops <fierceincii@gmail.com>
2023-12-01 22:18:23 +0200

    Try

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
-// server.js
-
-const { createServer } = require('https')
+const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const fs = require('fs');
-const path = require('path');
-const cron = require('node-cron');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync(path.resolve(__dirname, 'E:/private-key.pem')),
-  cert: fs.readFileSync(path.resolve(__dirname, 'E:/certificate.pem')),
-};
+const port = process.env.PORT || 3000;
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
+  createServer({ maxHeaderSize: 16384 }, (req, res) => {
     const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-    require('./src/lib/nodecron');
-
     handle(req, res, parsedUrl);
-  }).listen(3002, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log('> Ready on https://localhost:3002');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
