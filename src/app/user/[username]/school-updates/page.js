ff7dfc305c014417f8e88601eca82acb3e68f98f
SizoDevelops <fierceincii@gmail.com>
2023-12-12 14:47:55 +0200

    School Updates

--- src/app/user/[username]/school-updates/page.js
+++ src/app/user/[username]/school-updates/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from '@/components/HomePageCSS/schoolsUpdates.module.css'
+import SchoolUpdatesHome from '@/components/HomePage/School-Updates/SchoolUpdatesHome'
 export default function Page() {
   return (
     <body className={styles.container}>
-        
+        <SchoolUpdatesHome/>
     </body>
   )
 }
---
+++ src/components/HomePage/School-Updates/Message.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from '@/components/HomePage/School-Updates/SchoolCSS/message.module.css'
+export default function Message() {
+  return (
+    <div className={styles.cont}>
+        <div className={styles.message}>
+        <div className={styles.profile}>
+            <div className={styles.Image}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check" viewBox="0 0 16 16">
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+  <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
+            </svg>
+            </div>
+            <div className={styles.name}>
+                <h4>Tom Peter</h4>
+                <p>Teacher</p>
+            </div>
+        </div>
+        <p>Create Newsletters for your school <strong>Select from the wide range of templates</strong> offered by ScanA</p>
+        </div>
+
+        <div className={styles.time}>
+            <p>Today 07:03</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-text" viewBox="0 0 16 16">
+  <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
+  <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+            </svg>
+        </div>
+    </div>
+  )
+}
