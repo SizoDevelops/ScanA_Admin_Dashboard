ffaa5b4ebc14099716035323166b6352058c41a9
SizoDevelops <fierceincii@gmail.com>
2024-01-06 19:48:21 +0200

    This should work

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,22 @@ import Home from "@/components/HomePage/Home/Home";
 import HomePage from "@/components/HomePage/HomePage";
 
 import Loader from "@/components/shared/Loader";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 import { redirect, useRouter } from 'next/navigation';
-
+import { useLayoutEffect } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 const Page = () => {
  const {data: session, status} = useSession()
  const router = useRouter()
-
-  if (status === "loading") {
+ const [loading, setLoading] = useState(true)
+ useLayoutEffect(() => {
+  setLoading(false)
+ }, [])
+  if (status === "loading" || loading) {
     return <Loader />;
-  } else if (status === "authenticated"){
-    if(session.user?.school_name !== undefined){
-      redirect(`/user/${session.user?.school_name?.split(" ")[0]}${session.user?.school_name?.split(" ")[1]}`)
-    }
-    else {
-      signOut()
-    }
-      
-  } 
+  } else if (status === "authenticated") router.push(`/user/${session.user?.school_name?.split(" ")[0]}${session.user?.school_name?.split(" ")[1]}`)
    else  return (
       <>
         <Home />
