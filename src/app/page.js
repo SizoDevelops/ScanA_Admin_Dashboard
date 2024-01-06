c86ca6ee5433973d8736f3b55879061776dd6684
SizoDevelops <fierceincii@gmail.com>
2024-01-06 19:42:52 +0200

    OOps

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ const Page = () => {
     return <Loader />;
   } else if (status === "authenticated"){
     if(session.user?.school_name !== undefined){
-      redirect(`/user/${session.user?.school_name?.split(" ")[0]}-${session.user?.school_name?.split(" ")[1]}`)
+      redirect(`/user/${session.user?.school_name?.split(" ")[0]}${session.user?.school_name?.split(" ")[1]}`)
     }
     else {
       signOut()
