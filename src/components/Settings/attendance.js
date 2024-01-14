4d2ef72cdc10cd5e1f90094fe1cd23e3891b632e
SizoDevelops <fierceincii@gmail.com>
2024-01-14 15:40:43 +0200

    Atttendance

--- src/components/Settings/attendance.js
+++ src/components/Settings/attendance.js
@@ -42,13 +42,14 @@ const Attendance = () => {
     }
     else return (
         <body className={styles.container}>
-         <div className={styles.formHolder} ref={targetRef}>
-            <h2 className={styles.heading}>{getCurrentMonthAndYear()}</h2>
-            <h4 className={styles.heading}>{"WEEK " + getCurrentWeek()}</h4>
-            <Fab onClick={setAttendance} title="Refresh" color='info'> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+              <Fab onClick={setAttendance} title="Refresh" color='info'> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
   <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
   <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
 </svg></Fab>
+         <div className={styles.formHolder} ref={targetRef}>
+            <h2 className={styles.heading}>{getCurrentMonthAndYear()}</h2>
+            <h4 className={styles.heading}>{"WEEK " + getCurrentWeek()}</h4>
+          
             <p className={styles.details}>Scan the code for the day. If scanning does not work try entering the code.</p>
              <div className={styles.Grid}>
                 <div className={styles.QRCont} style={schema?.monday ? {display: "flex"} : {display: "none"}}>
