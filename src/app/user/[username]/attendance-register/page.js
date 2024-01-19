55a8c799db05a6d0a8eb2c956588b41bbf51952d
SizoDevelops <fierceincii@gmail.com>
2024-01-19 19:41:11 +0200

    Multiple Tables

--- src/app/user/[username]/attendance-register/page.js
+++ src/app/user/[username]/attendance-register/page.js
@@ -91,10 +91,10 @@ export default function ExcelPage() {
   const exportToExcel = async () => {
     // Create a new ExcelJS workbook
     const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('AttendanceSheet');
+    const worksheet = workbook.addWorksheet('Week'+sWeek.value);
 
     // Get the HTML table element by its ID
-    const table = document.getElementById('data-table');
+    const table = document.getElementById('data' + sWeek.value);
   
     // Iterate through the rows of the table and add them to the Excel worksheet
     const rows = table.querySelectorAll('tr');
@@ -147,6 +147,7 @@ export default function ExcelPage() {
     // Protect the workbook with a password
     await worksheet.protect(password, {selectLockedCells: false});
    
+
     // Save or serve the file as needed
     workbook.xlsx.writeBuffer().then((buffer) => {
       const blob = new Blob([buffer], { type: 'application/octet-stream' });
@@ -158,6 +159,89 @@ export default function ExcelPage() {
     // alert(password)
   };
 
+
+
+  const exportMultipleExcel = async () => {
+    // Create a new ExcelJS workbook
+    const workbook = new ExcelJS.Workbook();
+
+    const addSheets = async(id, sheet) => {
+      const worksheet = workbook.addWorksheet(sheet);
+
+      // Get the HTML table element by its ID
+      const table = document.getElementById(id);
+    
+      // Iterate through the rows of the table and add them to the Excel worksheet
+      const rows = table.querySelectorAll('tr');
+      rows.forEach((row) => {
+        const rowData = [];
+        const cells = row.querySelectorAll('td, th');
+    
+        cells.forEach((cell) => {
+          // Replace "-" with spaces in the cell content
+          const cellContent = cell.textContent.replace(/-/g, '  ');
+    
+          rowData.push(cellContent);
+        });
+    
+        worksheet.addRow(rowData);
+      });
+    
+      // Customize header row styles in ExcelJS
+      const headerRow = worksheet.getRow(1);
+      headerRow.eachCell((cell) => {
+        cell.font = { color: { argb: 'FFFFFF' } }; // Text color
+        cell.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: '03A4FF' } }; // Background color
+        cell.alignment = { horizontal: 'center', vertical: 'middle' }; // Center the text
+      });
+      headerRow.height = 30; // Adjust the height of the header row
+    
+      // Set a password for the Excel file
+      const password = voucher_codes.generate({
+        count: 1,
+        length: 11,
+    })[0]
+    
+      // Set uniform width for every cell
+      const columnCount = worksheet.columns.length;
+      for (let i = 1; i <= columnCount; i++) {
+        worksheet.getColumn(i).width = 30;
+        worksheet.getRow(i).height = 20;
+      }
+    
+      // Center the text in each cell
+      worksheet.eachRow({ startingRow: 2 }, (row) => {
+        row.eachCell((cell, num) => {
+          if(num !== 1){
+             cell.alignment = { horizontal: 'center', vertical: 'middle' }; // Center the text
+          }
+         
+        });
+      });
+    
+      // Protect the workbook with a password
+      await worksheet.protect(password, {selectLockedCells: false});
+     
+    }
+
+    week.forEach(async elem => {
+        await addSheets("data-table"+elem.value, "Week"+elem.value)
+    })
+    // Save or serve the file as needed
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], { type: 'application/octet-stream' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = `UserAttendance_${year.value}.xlsx`;
+      link.click();
+    });
+    // alert(password)
+  };
+
+
+
+
+  
  
   if(loading) {
     return <Loader/>
@@ -212,28 +296,46 @@ export default function ExcelPage() {
    
         <div  className={styles.DownloadActions}>
             <p className={styles.Date}>{"Week " + sWeek.value + " - " +year.value}</p> 
-                <div className={styles.buttons} onClick={exportToExcel}>
+            <div className={styles.btns}>
+                     <div className={styles.buttons} onClick={exportToExcel}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-arrow-down" viewBox="0 0 16 16">
-  <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
-  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
-</svg>
+                      <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+                      <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                    </svg>
                         <p>Download Excel</p>   
 
                     </div>
+
+                    <div className={styles.buttons} onClick={exportMultipleExcel}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-arrow-down" viewBox="0 0 16 16">
+                      <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+                      <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                    </svg>
+                        <p>Download All</p>   
+
+                    </div>
+            </div>
+         
                 </div> 
                 <div  className={styles.title}>
        <h1>Attendance Register</h1>
        <p className={styles.para}>This register is signed electronically. Thus the result are 100% authententic and adhare to the attendance policy of the organisation.</p>
         </div> 
     
-        
+  <table id={"data" + sWeek.value}  className={styles.TableHolder} >
+    <TableHeader   week={sWeek.value} position={sPosition.value} year={year.value}/> 
+  </table>
       
-
+{
+  week.map((item, index) => (
+    <table key={item}  id={"data-table" + item.value}  className={styles.visuallyHidden} >
+    <TableHeader   week={item.value} position={sPosition.value} year={year.value}/> 
+  </table>
+  ))
+}
         
      
-                <table   id="data-table"  className={styles.TableHolder}>
-                  <TableHeader   week={sWeek.value} position={sPosition.value} year={year.value}/> 
-                </table>
+               
       </div>
 
     </body>
