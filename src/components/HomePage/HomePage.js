10260f0580b52e508b4cb97240a776d952fb28cb
SizoDevelops <fierceincii@gmail.com>
2025-01-19 21:45:25 +0200

    Left Panel and Summary Done

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,16 +1,13 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import styles from '@/components/HomePageCSS/homePage.module.css'
-import Image from 'next/image'
 import DashProfile from '@/components/HomePage/dashProfile'
 import SidePanel from '@/components/HomePage/sidePanel'
 import { useSelector } from 'react-redux'
 import { signOut } from 'next-auth/react'
 import Loader from '../shared/Loader'
-// import { IconContext } from 'react-icons'
-// import { FiSearch } from "react-icons/fi";
-// import { RiLogoutCircleLine } from "react-icons/ri";
-
+import Image from 'next/image'
+import Logo from '../shared/Logo'
 
 export default function HomePage() {
 
@@ -38,85 +35,49 @@ if(schema.school_name === "" || !users) {
 }
 else return (
 
-    <body className={styles.container}>
-    
-     <div className={styles.panel}>
-         <SidePanel />
+    <div className={styles.container}>
+      <SidePanel/>
+      <div className={styles.content}>
+         <div className={styles.menu}>
+        <div className={styles.intro}>
+          <Logo bgColor={"#0099F1"} widthV={400} heightV={120} width={100} height={30}/>
+          <div className={styles.logout}>
+            <button onClick={() => {}}>Logout</button>
+          </div>
+        </div>
       </div>
-      <div className={styles.nav}>
-        
-<div className={styles.searchContainer}>
-  <div className={styles.nameHolder}>
-    <a className={styles.imageHolder} href='/' ></a>
-    <div className={styles.name}>
-      <p>Hello</p>
-      <h4>{schema?.school_admin?.admin_name.toUpperCase() || "User Name"}</h4>
-    </div>
-    <div title="Log Out" onClick={signOut}>
-    {/* <IconContext.Provider value={{color: "#03a4ff", style: { verticalAlign: 'middle' }}}>
-    <RiLogoutCircleLine className={styles.logOut}/>
-    </IconContext.Provider> */}
-    
-    <svg className={styles.logOut} fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"></path>
-</svg>
-    </div>
-   
-  </div>
-  <div className={styles.searchBar}>
-    <div >
-      {/* <Image alt="Search Icon" src="/icons/iconamoon_search.png" sizes='20' fill/> */}
-      {/* <IconContext.Provider value={{color: "#03a4ff", style: { verticalAlign: 'middle' }}}>
-    <FiSearch />
-    </IconContext.Provider> */}
-    <svg className={styles.searchIcon} fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
-</svg>
-    </div>
-    <input type='text' placeholder='Search' onChange={(e) => setSearch(e.target.value)}/>
-  </div>
-</div>
-<div className={styles.categories}>
-        {/* <div className={styles.cat} onClick={async () => {
-          
 
-          
-        }}>
-          <p>All</p>
-        </div>
-        <div className={styles.cat}>
-          <p>Admin</p>
-        </div>
-        <div className={styles.cat}>
-          <p>Teaching</p>
-        </div>
-        <div className={styles.cat}>
-          <p>Non-Teaching</p>
-        </div> */}
+      <div className={styles.summary}>
+        <Summary/>
+        <Summary/>
       </div>
-    <div className={styles.profiles}>
-        {
-            users.map((member, index) => {
-                return (
-                    <div key={member.last_name+index} style={{}}>
-                        <DashProfile slug={member.id} title={member.title} last_name={member.last_name} position={member.position} initial={member.initial} schema={member} image={"https://i.ibb.co/0csMj5v/Rectangle-353.png"} center={schema?.school_name} persal={member.persal}/>
-                    </div>
-                    
-                )
-            })
-          
-        }
-        
+      </div>
      
+
     </div>
-
-
-
-      </div>
-
-    </body>
 
     
   ) 
 }
 
+
+const Summary = () => {
+  return (
+    <div className={styles.sumDiv}>
+        <p className={styles.sumTitle}>Present Summary</p>
+
+        <div className={styles.summaryHeader}>
+          <div>
+            <p className={styles.header}>Present</p>
+            <p className={styles.present}>140</p>
+          </div>
+          <span/>
+          <div>
+            <p className={styles.header}>Absent</p>
+            <p className={styles.present}>140</p>
+          </div>
+        </div>
+
+      </div>
+  )
+}
