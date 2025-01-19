74aa7024ccfdde4dd5cc8eac1f62b5350932e304
SizoDevelops <fierceincii@gmail.com>
2025-01-19 19:33:17 +0200

    Removed Unnesessary Features

--- src/components/HomePage/topPanel.js
+++ src/components/HomePage/topPanel.js
@@ -14,7 +14,7 @@ export default function TopPanel() {
         title="Dashboard"
       >
         <div className={styles.sideIcon}>
-          {/* <Image sizes='30' fill src={"/icons/bi_folder.png"} alt="Folder Icon"/> */}
+        
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -29,12 +29,10 @@ export default function TopPanel() {
             />
           </svg>
         </div>
-        {/* <BsFolder /> */}
+      
         <p>Dashboard</p>
       </Link>
-      {/* <Link href={"/updates"} >
-    <Image sizes='30' fill src={"/icons/carbon_calendar.png"} alt="Folder Icon"/>
-    </Link> */}
+
       <Link
         href={`/user/${user.school_name
           .toLowerCase()
@@ -42,7 +40,7 @@ export default function TopPanel() {
           title="Attendance Register"
       >
         <div className={styles.sideIcon}>
-          {/* <Image sizes='30' fill src={"/icons/excel.png"} alt="Folder Icon"/> */}
+        
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -56,67 +54,11 @@ export default function TopPanel() {
           </svg>
         </div>
 
-        {/* <RiFileExcel2Line /> */}
+  
         <p>Attendance Register</p>
       </Link>
 
-      <Link
-        href={`/user/${user.school_name
-          .toLowerCase()
-          .replace(/\s+/g, "-")}/movement-register`}
-          title="Movement Register"
-      >
-        <div className={styles.sideIcon}>
-          {/* <Image sizes='30' fill src={"/icons/excel.png"} alt="Folder Icon"/> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-journal-x"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M6.146 6.146a.5.5 0 0 1 .708 0L8 7.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 8l1.147 1.146a.5.5 0 0 1-.708.708L8 8.707 6.854 9.854a.5.5 0 0 1-.708-.708L7.293 8 6.146 6.854a.5.5 0 0 1 0-.708"
-            />
-            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
-            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
-          </svg>
-        </div>
 
-        {/* <RiFileExcel2Line /> */}
-        <p>Movemwent Register</p>
-      </Link>
-
-      {/*Matric Learners */}
-      <Link
-        href={`/user/${user.school_name
-          .toLowerCase()
-          .replace(/\s+/g, "-")}/company-updates`}
-          title="Company Updates"
-      >
-        <div className={styles.sideIcon}>
-          {/* <Image sizes='30' fill src={"/icons/excel.png"} alt="Folder Icon"/> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="currentColor"
-            className="bi bi-journals"
-            viewBox="0 0 16 16"
-          >
-            <path d="M5 0h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2 2 2 0 0 1-2 2H3a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1H1a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v9a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1H3a2 2 0 0 1 2-2" />
-            <path d="M1 6v-.5a.5.5 0 0 1 1 0V6h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V9h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 2.5v.5H.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1H2v-.5a.5.5 0 0 0-1 0" />
-          </svg>
-        </div>
-
-        {/* <RiFileExcel2Line /> */}
-        <p>Company Updates</p>
-      </Link>
-      {/* <Link href={"/absentees"} >
-    <Image sizes='30' fill src={"/icons/iconoir_missing-font.png"} alt="Folder Icon"/>
-    </Link> */}
       <Link
         href={`/user/${user.school_name
           .toLowerCase()
@@ -124,8 +66,7 @@ export default function TopPanel() {
           title="Settings"
       >
         <div className={styles.sideIcon}>
-          {/* <Image sizes='30' fill src={"/icons/settings.png"} alt="Folder Icon"/>
-           */}
+     
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -138,12 +79,12 @@ export default function TopPanel() {
             <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z" />
           </svg>
         </div>
-        {/* <IoSettingsOutline /> */}
+ 
         <p>Settings</p>
       </Link>
-      <Link href={"#"} title="Help">
+      {/* <Link href={"#"} title="Help">
         <div className={styles.sideIcon}>
-          {/* <Image sizes='30' fill src={"/icons/carbon_help.png"} alt="Folder Icon"/> */}
+        
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="25"
@@ -156,9 +97,9 @@ export default function TopPanel() {
             <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
           </svg>
         </div>
-        {/* <IoIosHelpCircleOutline /> */}
+     
         <p>Help</p>
-      </Link>
+      </Link> */}
     </div>
   );
 }
