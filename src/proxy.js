5f3a9a686d801370318a2e0625ae06081cacc449
SizoDevelops <fierceincii@gmail.com>
2025-12-10 19:50:37 +0200

    No redirects on proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,11 +1,5 @@
-import { NextResponse } from "next/server";
-
 export { default } from "next-auth/middleware";
 
-// This function can be marked `async` if using `await` inside
-export function proxy(request) {
-  return NextResponse.redirect(new URL('/login', request.url))
-}
 export const config = {
   matcher: ["/user/:path*"],
 };
