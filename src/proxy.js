62e1f501df48b64d20f09d4ff0f85a0386c0ab57
SizoDevelops <fierceincii@gmail.com>
2025-12-10 19:47:45 +0200

    Wrong redirect on the proxy

--- src/proxy.js
+++ src/proxy.js
@@ -4,7 +4,7 @@ export { default } from "next-auth/middleware";
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.redirect(new URL('/login', request.url))
 }
 export const config = {
   matcher: ["/user/:path*"],
